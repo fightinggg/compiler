600abdc5f462ee7dc2335f83d94552a60d9abd88
wsx <246553278@qq.com>
2021-07-15 23:44:32 +0800

    -

--- src/main/java/grammar/GrammarOptimizer.java
+++ src/main/java/grammar/GrammarOptimizer.java
@@ -1,19 +1,48 @@
 package grammar;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author s
  */
 public class GrammarOptimizer {
 
+    Boolean findCanBeEmpty(Map<String,List<Production>> table,Set<String> canBeEmpty,Set<String> canNotEmpty,String target){
+        if(canBeEmpty.contains(target)){
+            return true;
+        }
+        if(canNotEmpty.contains(target)){
+            return false;
+        }
+        Boolean res = false;
+        if(target.length()<=1){
+            res = target.isEmpty();
+        }else{
+            for(Production production:table.get(target)){
+                if(production.getDerive().stream().allMatch(o->findCanBeEmpty(table,canBeEmpty,canNotEmpty,o))){
+                    res=true;
+                    break;
+                }
+            }
+        }
+        if(res){
+            canBeEmpty.add(target);
+        }else{
+            canNotEmpty.add(target);
+        }
+        return res;
+    }
 
 
     public static Grammar optimizer(Grammar grammar) {
         Map<String, List<Production>> productionsTable = grammar.getProductionsTable();
 
         // step.1 remove empty pattern
+        Set<String> canBeEmpty = new HashSet<>();
+        canBeEmpty
 
 
         // step.2 remove left dfs
