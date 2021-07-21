f66960f102c0ab3c53c1822f49d914d7b47789b9
hellowang <hellowang@tencent.com>
2021-07-21 20:50:58 +0800

    -

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+//package com.example;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        int x = 10000;
+//        int y = 200;
+//        int z = -200;
+//        System.out.println(new Integer(100)==new Integer(100));
+//        IndexOutOfBoundsException
+//    }
+//}
