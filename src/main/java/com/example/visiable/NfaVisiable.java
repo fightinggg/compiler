82a569f3223370a62992568a3554b5273102ed99
wsx <246553278@qq.com>
2021-07-28 08:02:27 +0800

    -

--- src/main/java/com/example/visiable/NfaVisiable.java
+++ src/main/java/com/example/visiable/NfaVisiable.java
@@ -6,6 +6,7 @@ import com.example.nfa.Nfa;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NfaVisiable {
 
@@ -15,13 +16,26 @@ public class NfaVisiable {
         nfa.allTrans().forEach((start, edges) -> edges.forEach((edge, toList) -> toList.forEach(to -> {
             String fromString = "\"%d\"".formatted(start.hashCode());
             String toString = "\"%d\"".formatted(to.hashCode());
-            edgeList.add(List.of(fromString, toString, edge));
+            edgeList.add(List.of(fromString, toString, JSON.toJSONString(edge)));
         })));
 
         String edgesString = edgeList.stream()
                 .collect(Collectors.groupingBy(strings -> List.of(strings.get(0), strings.get(1))))
                 .entrySet().stream()
-                .map(edge -> List.of(edge.getKey().get(0), edge.getKey().get(1), edge.getValue().stream().map(o -> o.get(2)).collect(Collectors.joining(","))))
+                .map(edge -> {
+                    final List<String> edgeSymbol = edge.getValue().stream().map(o -> o.get(2)).collect(Collectors.toList());
+                    edgeSymbol.sort(String::compareTo);
+                    final String collect = IntStream.range(0, edgeSymbol.size()).mapToObj(o -> o).collect(Collectors.groupingBy(new Function<Integer, Object>() {
+                        @Override
+                        public Object apply(Integer integer) {
+                            return integer / 10;
+                        }
+                    })).values().stream().map(o -> o.stream().map(c -> (char) c.intValue()).toList())
+                            .map(Object::toString)
+                            .map(o -> String.join(",", o))
+                            .collect(Collectors.joining("\n"));
+                    return List.of(edge.getKey().get(0), edge.getKey().get(1), collect);
+                })
                 .map(o -> " %s -> %s [ label = %s]".formatted(o.get(0), o.get(1), JSON.toJSONString(o.get(2))))
                 .collect(Collectors.joining("\n"));
 
